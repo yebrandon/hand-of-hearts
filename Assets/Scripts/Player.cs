4adcbebbcb5b8961b7cf8d9b742fd89f89cc56cd
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2021-02-08 20:03:28 -0500

    adding fighting functionality

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer m_SpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the SpriteRenderer from the GameObject
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        //Set the GameObject's Color quickly to a set Color (blue)
+        m_SpriteRenderer.color = Color.blue;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
