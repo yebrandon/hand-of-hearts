79af9f65c88aed4847624637469947283dfb5afc
18ask15 <71041208+18ask15@users.noreply.github.com>
2021-02-15 14:57:17 -0500

    scene changes at end of dialogue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -43,6 +43,7 @@ public class DialogueManager : MonoBehaviour
 
     void EndDialogue()
     {
+        FindObjectOfType<SwitchScene>().NextScene();
         Debug.Log("End of Conversation");
     }
 }
