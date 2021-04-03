f02ac46bbb65151ff6ddb11d2574a7a115159232
yebrandon <contactbrandonye@gmail.com>
2021-04-02 23:19:57 -0400

    implemented skeleton choose your lover scene

--- Assets/Scripts/Relationships.cs
+++ Assets/Scripts/Relationships.cs
@@ -3,10 +3,10 @@
 public static class Relationships
 {
     public static Dictionary<string, RelationshipStatus> relationships = new Dictionary<string, RelationshipStatus>(){
-        {"bff", RelationshipStatus.NOTMET},
+        {"Blossom", RelationshipStatus.NOTMET},
         {"Constants", RelationshipStatus.NOTMET},
         {"Candy", RelationshipStatus.NOTMET},
         {"Jibb", RelationshipStatus.NOTMET},
-        {"ruler", RelationshipStatus.NOTMET}
+        {"Rosa", RelationshipStatus.NOTMET}
     };
 }
